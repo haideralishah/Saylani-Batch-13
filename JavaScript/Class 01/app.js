b9e3fd602a6a6252d97cfd859f2f22c6cd7ca38a
haideralishah <headeralishah@gmail.com>
2025-01-24 15:58:15 +0500

    JS learning

---
+++ JavaScript/Class 01/app.js
@@ -0,0 +1,57 @@
+/*
+alert();
+console.log();
+
+data(information) types
+1. string ("" ''  ``)
+2. number
+
+*/
+
+// alert();
+// alert("Welcome Haider");
+// alert("Kese ho?");
+
+// console.log('Assalamaualikum');
+// console.log("Kese ho?");
+// console.log("walaikumassalam");
+// console.log("My phone number is: +92 305 9898769");
+
+// console.log(`My phone number is:
+//     +92 305 9898769`);
+
+// alert(`My phone number is:
+//     +92 305 9898769`);
+
+
+// console.log(
+//     'Quaid e Azam Said, "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."');
+
+
+
+// console.log(
+//     "Quaid e Azam Said, 'Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.'");
+
+
+
+
+// alert("My name is Haider.");
+
+
+// alert("haider");
+// aler('abdullah');
+// alert("ahmed");
+// console.log("03413542800 + 1");
+// console.log(5000);
+
+
+console.log(1 + 3); // 4
+console.log(1 - 3); // -2
+console.log(2 * 3); // 6
+console.log(6 / 3); // 2
+console.log(2 * 4 + 3); //11
+
+console.log(1 + 1);  //2
+console.log("1" + "1");
+
+console.log("Welcome" + " " + "Haider"); //Welcome Haider
