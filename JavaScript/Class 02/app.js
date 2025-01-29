c53ec490053e044420f773df458ce056ecbe8306
haideralishah <headeralishah@gmail.com>
2025-01-29 14:09:28 +0500

    JS chapter 5

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,320 @@
+/*
+    string "" '' ``
+    number
+
+    alert();
+    window.alert();
+
+    console.log();
+
+    document.write();
+
+    prompt();
+
+    let is used to declare variable.
+
+    + - * / % **
+    ++
+    --
+*/
+
+
+// alert("haider");
+// alert("haider");
+
+// alert(" haider _ - + * /");
+
+
+
+
+// document.write("<h3>Hello Mars</h3>");
+
+// document.write(" Assalamualikum <br />");
+
+// document.write(5 + 3);
+
+// document.write("<br /> Haider" + " " + "Ali");
+
+// console.log("<h3>Hello Mars</h3>");
+
+// alert("<h3>Hello Mars</h3>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ==============  */
+
+// let user;
+// user = "haider";
+// console.log(user);
+
+// user = "ali";
+// console.log(user);
+
+// user = "Bilal Ahmed"
+// document.write('Welcome ' + user + "<br />");
+
+// document.write(user + ', you are welcome.' + "<br />");
+
+// document.write(user + ', your fee is due.' + "<br />");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName, lastName, surname;
+
+// firstName = 'haider', lastName = 'ali', surname = 'shah';
+
+// console.log(firstName, lastName, surname);
+
+
+
+
+
+
+
+
+
+// let firstName;
+// firstName = "haider";
+
+// let firstName = "Haider";
+// let lastName = "Ali";
+// let surname = "Shah";
+
+
+
+
+
+
+
+
+
+// let greet = "Assalamualikum";
+// let userName = "Haider Ali";
+
+// alert(greet + ", " + userName);
+
+
+
+
+
+// let num1 = 500;
+// let num2 = 300;
+
+// console.log(num1 + num2);
+
+
+
+
+
+
+
+// let fee = 5000;
+// let discountPercent = 10;
+
+// let discountAmount = fee / 100 * discountPercent;
+
+// let dueFee = fee - discountAmount;
+
+// console.log(dueFee);
+
+
+
+
+
+
+
+
+
+
+
+// prompt("What is your name?");
+// prompt("Where do you belong to?", "Karachi");
+
+
+// let userName = prompt("What is your name?");
+// console.log("Welcome", userName);
+
+// let donationAmount = +prompt("How much do you want to donate?");
+
+// console.log(donationAmount + 10);
+// console.log(5000);
+
+
+
+
+
+
+// let age = prompt('what is your age?');
+
+// alert("I am " + age + " years old.");
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 3;
+// console.log(num1 ** 3);
+
+// console.log(11 % 2);
+
+// console.log(49 % 5);
+
+
+
+// let num1 = +prompt("enter a number please.");
+// console.log(num1 % 2);
+
+
+
+// let turnNumber = 1;
+// let count = 10;
+
+
+// console.log(turnNumber++);
+// console.log(turnNumber++);
+// console.log(turnNumber++);
+
+// console.log(count--);
+// console.log(count--);
+// console.log(count--);
+
+
+
+
+
+
+
+/**/
+
+
+
+
+
+
+
+
+// let tableName = +prompt("What table do you want me to print?"); // 2
+// let count = 1;
+
+// // 2 x 1 = 2
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+// console.log(tableName + " x " + count + " = " + count++ * tableName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 5;
+// let num2 = 7;
+// //             6   +   7    +   6    -   8
+// console.log(++num1 + num2++ + num1++ - ++num1);
+
+//            5    +   7    +   6   -   6
+// console.log(num1++ + ++num1 + --num2 - num2--);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num1 = 4;
+
+num1 = num1 + 2;
+
+num1 += 2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
