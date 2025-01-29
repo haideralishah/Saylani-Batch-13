823cf3e7683b38feb4d6cdbcdfbd01017d1f99df
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-01-29 16:12:11 +0500

    lab 1 practie

---
+++ JavaScript/Lab/Lab 1/app.js
@@ -0,0 +1,106 @@
+/*
+alert();
+console.log();
+document.write();
+prompt();
+
+let;
+typeof
+
+*/
+
+// let greet = prompt("Which greet do you want to receive?", "Assalamualaikum");
+// let userName = prompt('what is your name?');
+
+// console.log(greet + " " + userName);
+
+
+
+/*
+use alert
+
+PIZZA
+PIZZ
+PIZ
+PI
+P
+
+*/
+
+
+// let food = `
+// PIZZA
+// PIZZ
+// PIZ
+// PI
+// P
+// `;
+
+// alert(food);
+// console.log(food);
+// document.write(food);
+
+
+// let food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+// console.log(food);
+// document.write(food);
+
+
+
+
+
+
+
+
+// let jobTitle = "Presient";
+// let country = "USA";
+// let partner = "x";
+// let children = 2;
+
+// alert("You will be a " + jobTitle + " in " + country + " , and married to " + partner + " with " + children + " kids." );
+
+
+
+
+
+
+
+
+// let birthYear = +prompt("Plese enter your birth year.");
+
+// document.write("My birth year is " + birthYear + ".");
+// document.write("<br />");
+// document.write("Data type of my declared variable is " + typeof birthYear + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+2
+5
+The sum of two number is: 7
+The substract of two number is: -3
+The product of two number is: 10
+The division of two number is: .4
+The modulus of two number is: 5
+*/
+
+let num1 = +prompt('enter a number');
+let num2 = +prompt('enter another number');
+
+document.write("The sum of two number is: " + (num1 + num2));
+document.write("The sum of two number is: ", num1 + num2);
+
+
