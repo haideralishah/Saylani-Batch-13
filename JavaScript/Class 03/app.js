4191e87d108d52b0027eb489a654ed5264bc75e3
haideralishah <headeralishah@gmail.com>
2025-01-31 16:05:41 +0500

    if intro

---
+++ JavaScript/Class 03/app.js
@@ -0,0 +1,190 @@
+/*
+++variableName
+variableName++
+
+--variableName
+variableName--
+
+
+boolean; true false
+===  equal to
+!==  not equal to
+>   greater than
+<   less than
+>=  greater than or equal to
+<=  less than or equal to
+
+
+
+if(condition){
+    code block;
+}
+
+*/
+
+
+
+
+// let num1 = 5;
+// let num2 = 7;
+
+// console.log(num1++);
+// console.log(++num1);
+
+// console.log(num2--); //7
+// console.log(--num2); //5
+
+
+
+
+
+
+// let table = +prompt('please enter a table name');
+// let count = 1;
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+// console.log(
+//     table + " x " + count + " = " + table * count++
+// );
+
+
+
+
+
+
+
+
+
+
+// let num1 = 9;
+// num1 = num1 + 2;
+// console.log(num1); // 11
+
+// num1 = num1 - 5;
+// console.log(num1); // 6
+
+// num1 = num1 * 3; //18
+// console.log(num1);
+
+
+
+
+// let frstChr = "H";
+// frstChr = frstChr + "A";
+// console.log(frstChr); //HA
+
+
+
+// let frstChr = "5";
+// frstChr = frstChr + "4";
+// console.log(frstChr);
+
+
+
+
+
+// let frstChr = "5";
+// frstChr = frstChr / "2";
+// console.log(frstChr, "2.5");
+
+
+
+
+
+// let equation = 2 * (5 + (3 - 2 * 9 / 3));
+// console.log(equation);
+
+
+
+
+
+
+// console.log(true);
+
+// console.log(false);
+
+
+
+// let eligible = confirm("Are you older than 14?");
+// console.log(eligible);
+
+
+
+// let eligible = confirm("are you graduate?");
+// console.log(eligible);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName = prompt("what is your name?");
+// let blacklisted = "haider";
+// console.log(userName === blacklisted);
+
+// let num1 = 9;
+// let num2 = 9;
+
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 === num2); 
+
+
+
+
+
+
+// let age = prompt('what is your age?');
+// let eligiblity = 14;
+
+// if (age <= eligiblity) {
+//     console.log('You are eligible for our sports club.');
+// }
+
+
+
+
+let blacklisted = "haider";
+let userName = prompt('what is your name?');
+
+if (blacklisted === userName) {
+    console.log('you are block from our website.');
+}
+
+
+
+
+
+
