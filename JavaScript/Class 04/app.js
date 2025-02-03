318bca22174313069462dea8a976a46bfe3d9818
haideralishah <headeralishah@gmail.com>
2025-02-03 15:47:48 +0500

    if else if and else conditions

---
+++ JavaScript/Class 04/app.js
@@ -0,0 +1,264 @@
+/*
+Boolean true false
+===
+!==
+<
+>
+<=
+>=
+
+&&  and
+||  or
+
+
+*/
+
+
+
+
+// let isMetriculate = confirm('kia apne matric kia h?');
+
+// if (isMetriculate) {
+//     console.log("Congrats! you are hired.");
+// }
+
+
+
+
+
+// let reqQuali = 'metric';
+// let qualificaiton = prompt('what is your qualification?');
+
+// if (reqQuali === qualificaiton) {
+//     console.log('you are hired');
+// }
+
+
+
+
+
+
+
+
+// let shopName = prompt('which shop is open?');
+
+// if (shopName === 'faisal') {
+//     console.log('bring chicken');
+// }
+// else if (shopName === 'muzammil') {
+//     console.log('bring daal');
+// }
+
+
+
+
+
+
+// let secondShop = prompt('which shop is open?');
+
+// if (secondShop === 'faisal') {
+//     console.log('bring chicken');
+// }
+// if (secondShop === 'muzammil') {
+//     console.log('bring daal');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let qualificaiton = prompt('what is your qualification?');
+
+// if (qualificaiton === 'Ph.D') {
+//     console.log('10 lac');
+// }
+// else if (qualificaiton === 'masters') {
+//     console.log('7 lac');
+// }
+// else if (qualificaiton === 'graduate') {
+//     console.log('4 lac');
+// }
+// else if (qualificaiton === 'intermediate') {
+//     console.log('50000');
+// }
+// else if (qualificaiton === 'metric') {
+//     console.log('free internship');
+// }
+
+
+
+
+
+
+// let age = +prompt('what is your age?');
+
+// if (age > 15) {
+//     console.log('you are eligible for cricket.');
+// }
+// else if (age > 12) {
+//     console.log('you are eligible for hocky.');
+// }
+// else {
+//     console.log('you are eligible for running practice');
+// }
+
+
+
+
+
+
+
+
+
+/* assignments */
+
+// let num = +prompt('enter a number');//4
+// if (num % 3 === 0) {
+//     console.log('divisible by number');
+// }
+// else {
+//     console.log('not divisible by number');
+// }
+
+
+
+// let num = +prompt('enter a number');//4
+
+// if (num % 2 === 0) {
+//     console.log('even');
+// }
+// else {
+//     console.log('odd');
+// }
+
+
+// let myName = "haider";
+// let userName = prompt('what is your name?');
+
+// if (myName === userName) {
+//     console.log('hey! we have similar name.');
+// }
+
+
+
+
+
+// let num1 = prompt('enter a number');
+// let num2 = prompt('enter another number');
+// let operator = prompt('enter operation');
+
+
+/* assignments */
+
+
+
+
+
+
+
+
+
+
+// let reqQuali = prompt("What is your qualification?");
+// let domecile = prompt('where is your domicile from?');
+
+// if (reqQuali === 'metric' && domecile === 'sindh') {
+//     console.log('congrats you are eligible for head of the department.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = +prompt('what is your age?');
+// let gender = prompt('enter your gender');
+
+// if (age > 15 && gender === 'male') {
+//     console.log('you are eligible for our sports club');
+// }
+// else {
+//     console.log('you are not eligible');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isSpeciale = confirm('are you a special person?');
+// let city = prompt('where do you live?');
+
+// if (city === 'karachi' || isSpeciale) {
+//     console.log('you are eligible for admission.');
+// }
+
+
+
+
+
+
+
+let areYouMemon = confirm('do you belong to memon community?')
+let age = prompt("enter you age");
+let zakatElible = confirm('are you elibible for zakat');
+
+if ((areYouMemon && age < 15) || zakatElible) {
+    console.log('you are awarded 20% discount.');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
