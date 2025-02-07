2ba1c4001b30e8047ba8d8384687434bff77f4bd
haideralishah <headeralishah@gmail.com>
2025-02-07 15:54:10 +0500

    array intro

---
+++ JavaScript/Class 05/app.js
@@ -0,0 +1,182 @@
+/*
+nested conditions
+`  ${}`
+undefined;
+Array
+
+array[index];
+
+array.push();
+array.unshift()
+array.pop()
+array.shift()
+
+*/
+
+
+// let ranNum = 10000;
+// let score = +prompt('what is your score?');
+// let isSpecial = confirm("are you specially abled person?");
+// if (isSpecial) {
+//     if (score > 65) {
+//         console.log("Your salary is:", ranNum * 2);
+//     }
+//     else if (score > 55) {
+//         console.log("Your salary is:", ranNum * 1.5);
+//     }
+// }
+// else {
+//     if (score > 65) {
+//         console.log("Your salary is:", ranNum * 1.75);
+//     }
+//     else if (score > 55) {
+//         console.log("Your salary is:", ranNum * 1.25);
+//     }
+// }
+
+
+// let x = 5;
+// let y = 9;
+// if (x > a) {
+//     if (x > a) {
+//         console.log("a");
+//     }
+//     else if (y <= a) {
+//         console.log("b");
+//     }
+// }
+// else if (y > a) {
+//     if (y <= a) {
+//         console.log("c");
+//     }
+//     else if (y > a) {
+//         console.log("d");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isItEid = confirm('Is it Eid today?');
+
+// if (isItEid) document.write('Eid Mubarak');
+// else document.write('Assalamualaikum');
+
+
+
+
+
+
+
+
+
+// let firstName = "Haider";
+// let lastName = "Ali";
+// let message = `My name is ${firstName} ${lastName}`;
+// console.log(message);
+
+// let food1 = 500;
+// let food2 = 700;
+// let bill = `Your total bill is ${food1 + food2}`;
+// console.log(bill);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a;
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Oranges"];
+// console.log(fruits);
+
+// let luckyNumbers = [500, 100, 700, 900];
+// console.log(luckyNumbers);
+
+// let mixArr = ['Apple', 'Banana', 1, 9, 11, 200, false, ['USA', "Pakistan", "Bangladesh"]];
+// console.log(mixArr);
+
+// let firstName = "haider";
+// let lastName = "ali";
+
+// let fullName = [firstName, lastName];
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Oranges"];
+// console.log(fruits[2]);
+// console.log(fruits[4]);
+// console.log(fruits[0]);
+
+// let countries = ['USA', "Pakistan", "Bangladesh"];
+// let mixArr = ['Apple', 'Banana', 1, 9, 11, 200, false, countries];
+// console.log(mixArr[7][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let veggies = [];
+
+// veggies[0] = "potato";
+// veggies[2] = "tomato";
+veggies.push("potato"); //add in last
+veggies.push('tomato');
+veggies.unshift('lady finger');//add in start
+veggies.unshift('cabbage');
+
+veggies.pop();//delete last 
+veggies.shift();//delete first 
+console.log(veggies);
+
+
+
