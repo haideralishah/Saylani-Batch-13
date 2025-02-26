9409c962b6952b50f75f885e7c5fd5bf586fbad9
JaffarAman <amanjaffar50@gmail.com>
2025-02-26 15:57:56 +0500

    DOM and functions

---
+++ JavaScript/Lab/Lab 5/app.js
@@ -0,0 +1,68 @@
+// let h1 = document.querySelectorAll('h1')[0];
+
+// function greetUser() {
+//     let userName = prompt('what is your name?');
+
+//     h1.innerHTML = userName;
+// }
+
+
+
+
+
+
+// let h1 = document.querySelectorAll(".captain")[0];
+// let captains = ['Sarfaraz', 'Babar', 'Rizwan'];
+
+// function changeCaptain() {
+//     let currentCap = h1.innerHTML;
+//     let currentCapIdx = captains.indexOf(currentCap); 
+//     let nextCapIdx = currentCapIdx + 1; 
+//     if (nextCapIdx === captains.length) {
+//         nextCapIdx = 0;
+//     }
+//     h1.innerHTML = captains[nextCapIdx];
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let imgEl = document.querySelectorAll('img')[0];
+// imgEl.style.width = '300px';
+// imgEl.style.height = '300px';
+// let is5Rupee = true;
+// function changeCoinFace() {
+//     if (is5Rupee) {
+//         is5Rupee = false;
+//         imgEl.src = './images/chand.jpg'
+//     }
+//     else {
+//         is5Rupee = true;
+//         imgEl.src = './images/head5.webp'
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let colors = ['red', 'blue', 'green', 'yellow'];
