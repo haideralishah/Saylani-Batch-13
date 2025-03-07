97115ba6c34dfa93543a1a42c3cca2b6026fb8fc
haideralishah <headeralishah@gmail.com>
2025-03-07 15:03:45 +0500

    date topic covered

---
+++ JavaScript/Class 13/app.js
@@ -0,0 +1,110 @@
+// let now = new Date();
+// console.log(now);
+
+// let day = now.getDay();
+// let daysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', "Friday", "Saturday"]
+// console.log(day);
+// console.log(daysName[day]);
+
+// let month = now.getMonth();
+// let monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// console.log(month);
+// console.log(monthsName[month]);
+
+// let currentDate = now.getDate();
+// let fullYear = now.getFullYear();
+// let hours = now.getHours();
+// let minute = now.getMinutes();
+// let second = now.getSeconds();
+// let ms = now.getMilliseconds();
+// console.log(currentDate, fullYear, hours, minute, second, ms);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dob = new Date("October 4, 1991");
+// console.log(dob);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let now = new Date();
+// let msTime = now.getTime();
+// console.log(msTime);
+
+
+
+
+
+
+
+
+
+
+
+
+// let doomsDay = new Date("June 30, 2035");
+// let today = new Date();
+
+// let msDoomsDay = doomsDay.getTime();
+// let msToday = today.getTime();
+
+// let msRemaining = msDoomsDay - msToday;
+// console.log(Math.floor(msRemaining / 1000 / 60 / 60 / 24 / 365.25))
+
+
+
+// let today = new Date();
+// let classStart = new Date("September 23, 2024");
+
+// let passedTime = today - classStart;
+
+// console.log(passedTime / 1000 / 60 / 60 / 24 / 30);
+
+
+
+
+
+
+
+
+let now = new Date();
+
+now.setHours(9);
+now.setDate(23);
+now.setMinutes(30);
+
+console.log(now);
+
+
+
