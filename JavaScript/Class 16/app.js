4ce0986e574d16fb38be331cfed984735f806183
haideralishah <headeralishah@gmail.com>
2025-03-17 16:32:33 +0500

    JS functions

---
+++ JavaScript/Class 16/app.js
@@ -0,0 +1,255 @@
+
+// let firstRamadan = new Date("March 2, 2025");
+// let today = new Date();
+
+// let passedDaysinMS = today - firstRamadan;
+// let passedDays = Math.floor(passedDaysinMS / 1000 / 60 / 60 / 24);
+
+// console.log(passedDays);
+
+
+
+
+
+
+
+
+// function computePower(base, power) {
+//     console.log(base ** power);
+// }
+// computePower(2, 3);
+// computePower(3, 3);
+// computePower(5, 4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let vowels = ["a", "e", "i", "o", "u"];
+
+// function countVowels(str) {
+//     let vowelsCount = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i]).toLowerCase()) {
+//             vowelsCount++;
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = "haider";
+// console.log(user);
+// function greetUser() {
+//     user = "abdullah";
+//     console.log(user);
+// }
+
+// console.log(user);
+// greetUser();
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// greetUser();
+
+
+// let user = "haider";
+// console.log(user); //haider
+// function greetUser() {
+//     let user = "abdullah";
+//     console.log(user); //abdullah
+// }
+
+// console.log(user); // haider
+// console.log(user); // haider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = "haider";
+// console.log(user);
+// function greetUser() {
+//     console.log(user);
+//     user = "abdullah";
+//     console.log(user);
+// }
+// console.log(user);
+// greetUser()
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = "haider";
+// function printuser() {
+//     let user = 'abdullah';
+//     if (user === 'abdullah') {
+//         let user = 'muzammil';
+//         console.log(user);
+//     }
+//     console.log(user);
+// }
+// printuser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = "haider";
+// function printuser() {
+//     let user;
+//     if (user === 'abdullah') {
+//         user = 'muzammil';
+//         console.log(user);
+//     }
+//     else {
+//         user = 'ahmed';
+//         console.log(user);
+//     }
+//     console.log(user);
+// }
+// printuser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
