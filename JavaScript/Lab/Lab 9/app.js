9b91b86003bdc2e91f72b273e3bf1fae6ce22e9f
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-04-09 15:09:10 +0500

    delete todo template

--- JavaScript/Lab/Lab 9/app.js
+++ JavaScript/Lab/Lab 9/app.js
@@ -1,16 +1,32 @@
 let todos = [];
 let olEl = document.querySelectorAll('.all-todos')[0];
+let newTodoEl = document.querySelectorAll('#new-todo')[0];
 
-function renderTodos(){
+function renderTodos() {
     olEl.innerHTML = "";
-    for(let i =0; i < todos.length; i++){
+    for (let i = 0; i < todos.length; i++) {
         let todoItem = todos[i];
-        let liEl = `<li>${todoItem}</li>`;
+        let liEl = `<li>
+                        ${todoItem}
+                        <button onclick="deleteTodo(${i})">
+                            Delete
+                        </button>
+                    </li>`;
+
+                    
         olEl.innerHTML += liEl;
     }
 }
 
-function addTodo(){
-    // add new todo in array
-    //call renderTodos to print updated array
+function addTodo() {
+    let newTodo = newTodoEl.value;
+    todos.push(newTodo);
+    renderTodos();
+    newTodoEl.value = "";
+}
+
+function deleteTodo(index) {
+    console.log(index);
+    // splice receiving index 
+    // call renderTodos
 }
