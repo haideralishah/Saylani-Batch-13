932d5c5c28e63018a90f3f1d70d681db6a052620
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-04-16 14:28:21 +0500

    cars object for lab task

---
+++ JavaScript/Lab/Lab 10/app.js
@@ -0,0 +1,35 @@
+
+let cars = [
+    {
+        name: 'Lamborghini',
+        model: "Urus",
+        color: 'Orange',
+        price: 100000,
+        imgSrc: "https://hips.hearstapps.com/hmg-prod/images/2025-lamborghini-urus-se-phev-106-67005496322ba.jpg?crop=0.633xw:0.534xh;0.223xw,0.427xh&resize=1200:*",
+        isImported: true
+    },
+    {
+        name: 'Mehran',
+        model: "VX",
+        color: 'Silver',
+        price: 2500,
+        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJtBX7b_ukY1rNcPgP_Y7BzG2AwWJecct6Q&s",
+        isImported: false
+    },
+    {
+        name: 'Sonata',
+        model: "N Line",
+        color: 'White',
+        price: 60000,
+        imgSrc: "https://propakistani.pk/wp-content/uploads/2025/01/N-Line.jpg",
+        isImported: true
+    },
+    {
+        name: 'Havel',
+        model: "H6",
+        color: 'black',
+        price: 40000,
+        imgSrc: "https://i.ytimg.com/vi/wAC15UwhCXo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBUbNTquKFO2W1t_bhbheKPMdYgrg",
+        isImported: false
+    }
+];
