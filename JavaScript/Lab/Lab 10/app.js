322d6e623b51556d8a16aa0efc1689f22382d79d
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2025-04-16 15:57:08 +0500

    cars listing

--- JavaScript/Lab/Lab 10/app.js
+++ JavaScript/Lab/Lab 10/app.js
@@ -1,10 +1,10 @@
 
-let cars = [
+let carsData = [
     {
         name: 'Lamborghini',
         model: "Urus",
         color: 'Orange',
-        price: 100000,
+        price: 300000,
         imgSrc: "https://hips.hearstapps.com/hmg-prod/images/2025-lamborghini-urus-se-phev-106-67005496322ba.jpg?crop=0.633xw:0.534xh;0.223xw,0.427xh&resize=1200:*",
         isImported: true
     },
@@ -33,3 +33,48 @@ let cars = [
         isImported: false
     }
 ];
+
+
+let carListEl = document.querySelectorAll(".cars-list-container")[0];
+
+function listCars(cars) {
+    for (let i = 0; i < cars.length; i++) {
+        let carCard = makeCarCard(cars[i]);
+        carListEl.innerHTML += carCard;
+    }
+}
+
+listCars(carsData);
+
+
+function makeCarCard(car) {
+    return `<div>
+                <img src="${car.imgSrc}" alt="${car.name}"/>
+                <h2>${car.name} ${car.model}</h2>
+                <p>${car.color}</p>
+                <p>${car.price}</p>
+            </div>
+        `
+}
+
+
+function filterCars(el) {
+    console.log(el.value);
+    let filterHoiwiCars = [];
+    for (let i = 0; i < carsData.length; i++) {
+        if (el.value === 'Imported' && carsData[i].isImported) {
+            filterHoiwiCars.push(carsData[i]);
+        }
+        else if (el.value === 'Pakistani' && !carsData[i].isImported) {
+            filterHoiwiCars.push(carsData[i]);
+
+        }
+    }
+    if(el.value === 'All'){
+        filterHoiwiCars = carsData.slice(0);
+    }
+
+    carListEl.innerHTML = "";
+    listCars(filterHoiwiCars);
+
+}
